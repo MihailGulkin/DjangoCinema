563ef68128addaf0adb9a758b5f4961a04b087bb
Bol4onok <Bol4onok@mail.com>
2022-09-07 00:53:21 +0300

    3 commit styles

--- web/models.py
+++ web/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Director(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    director = models.ForeignKey('Director', on_delete=models.CASCADE,
+                                 default=None)
+    release_date = models.CharField(max_length=70, default='None')
+    short_intro = models.TextField(max_length=700)
+
+    IMDb_RATING = models.CharField(max_length=50, default=None)
+    genre = models.ManyToManyField('Genre')
+    poster = models.ImageField(upload_to='Movie/Posters/')
+    movie_page_poster = models.ImageField(upload_to='Movie/Posters/MoviePage/',
+                                          null=True, blank=True)
+    summary = models.TextField(max_length=1600)
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created']
--- web/views.py
+++ web/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.views import View
-
+from .models import Movie
 
 class MainPageView(View):
     template = 'web/main.html'
-
+    movie_model = Movie.objects.all()
     def get(self, request):
-        return render(request, self.template)
+        return render(request, self.template, {"movies": self.movie_model})
